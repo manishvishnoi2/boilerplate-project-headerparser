dd05e6bf51204cda6f8bfba73a69ebeb8392a20f
Manish Vishnoi <manish.vishnoi2@gmail.com>
2019-06-01 23:35:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/whoami", function(req, res){
+  res.json({"ip-address":req.header('x-forwarded-for').split(',')[0], "Language":req.header('Accept-Language'), "software": req.header('User-Agent')})
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
